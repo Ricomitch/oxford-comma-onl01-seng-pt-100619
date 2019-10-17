4df51d1329499dbc291c5341a0368e95a53be671
Rico Mitchell <ricomitch@icloud.com>
2019-10-17 03:04:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,11 +5,8 @@ def oxford_comma(array)
     return array[0]
   elsif array.size == 2
     return array.join(" and ")
-  else
-    array.insert(-2, " and ").join(", ")
+  elsif array.length >= 3
+    return array[0..-2].join(', ') + ", and " + array[-1]
   end
 #binding.pry
 end
-elsif array.length >= 3
-    return array[0..-2].join(', ') + ", and " + array[-1]
-  end
