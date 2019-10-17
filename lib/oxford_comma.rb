0d877d08a621372edd6b2b375eb952a909256820
Rico Mitchell <ricomitch@icloud.com>
2019-10-17 02:41:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array.insert(-2, " and ").join(", ")
+  end
+#binding.pry
 end
