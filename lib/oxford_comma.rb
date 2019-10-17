011334a2605dbe47acc86104fd07aa21eeaeaa4c
Rico Mitchell <ricomitch@icloud.com>
2019-10-17 02:57:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
   else
-    array.insert(-2, " and ").join(", ")
+    array(-2, " and ").join(", ")
   end
 #binding.pry
 end
